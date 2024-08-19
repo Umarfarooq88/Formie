48389918ec350da05dee39a7f2a6b6da29eae82f
Umarfarooq88 <umarat372@gmail.com>
2024-08-19 12:47:47 +0530

    Add Form fields

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import{MatDialog} from '@angular/material/dialog'
+import { EmpAddEditComponent } from './emp-add-edit/emp-add-edit.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Formie';
+
+  constructor(private _dialog : MatDialog){}
+
+  openAddEditEmpForm(){
+    this._dialog.open(EmpAddEditComponent);
+  }
+
 }
